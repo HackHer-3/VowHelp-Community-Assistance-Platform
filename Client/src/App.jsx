04b8afa3c35c706f9f0443c0b4c8ea3c0741f2c6
Dharshinisri-G <dharshinisrigajendran@gmail.com>
2024-10-19 14:07:35 +0530

    Tried route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home"
 import Signin from "./pages/SignIn"
+import { Route,Routes } from "react-router-dom"
 function App() {
    
   return (
     <>
+
      <Home/>
         {/* <Signin/> */}
     </>
